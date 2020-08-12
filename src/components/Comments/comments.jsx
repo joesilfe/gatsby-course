562b9b10435457059ea769d975e74fe4a0183f46
unknown <joel.silva.ferr@gmail.com>
2020-08-12 00:04:24 -0300

    styled rename archive name

--- src/components/Comments/comments.jsx
+++ src/components/Comments/comments.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDisqusComments from 'react-disqus-comments';
 
-import * as S from './CommentsCss';
+import * as S from './commentsCss';
 
 const Comments = ({ url, title }) => {
     // Quando tiver um url do seu servido adicione na variável abaixo
